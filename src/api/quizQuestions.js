c10b4664e3ae8edd63ce068c4d1a540b587077d7
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-05-28 17:03:57 +0200

    - start
    - upd questions
    
    - upd results

---
+++ src/api/quizQuestions.js
@@ -0,0 +1,46 @@
+var quizQuestions = [
+  {
+      question: "Aufgabe 1. In Deutschland dürfen Menschen offen etwas gegen die Regierung sagen, weil ... - hier Religionsfreiheit gilt.",
+      answers: [
+          {
+              type: "You level is A1",
+              content: "hier Religionsfreiheit gilt"
+          },
+          {
+              type: "You level is A2",
+              content: "die Menschen Steuern zahlen."
+          },
+          {
+              type: "You level is B1",
+              content: "die Menschen das Wahlrecht haben."
+          },
+          {
+              type: "You level is B2",
+              content: "hier Meinungsfreiheit gilt. <<<= richtige Antwort"
+          }
+      ]
+  },
+  {
+      question: "In Deutschland können Eltern bis zum 14. Lebensjahr ihres Kindes entscheiden, ob es in der Schule am ...",
+      answers: [
+          {
+              type: "You level is A1",
+              content: "Geschichtsunterricht teilnimmt."
+          },
+          {
+              type: "You level is A2",
+              content: "Religionsunterricht teilnimmt."
+          },
+          {
+              type: "You level is B1",
+              content: "Politikunterricht teilnimmt."
+          },
+          {
+              type: "You level is B2",
+              content: "Sprachunterricht teilnimmt."
+          }
+      ]
+  }
+];
+
+export default quizQuestions;
