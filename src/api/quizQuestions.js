d3524d7a18ed1672d8cb918811c13d68523c24e8
Evgeny Bazhanov <evg.bazhanov@yahoo.com>
2020-07-20 01:43:13 +0200

    - upd quizQuestions.js

--- src/api/quizQuestions.js
+++ src/api/quizQuestions.js
@@ -3,19 +3,19 @@ var quizQuestions = [
         question: "In Deutschland dürfen Menschen offen etwas gegen die Regierung sagen, weil ... - hier Religionsfreiheit gilt.",
         answers: [
             {
-                type: "falsche Antwort",
+                type: "your level A1",
                 content: "hier Religionsfreiheit gilt"
             },
             {
-                type: "falsche Antwort",
+                type: "your level A2",
                 content: "die Menschen Steuern zahlen."
             },
             {
-                type: "falsche Antwort",
+                type: "your level B1",
                 content: "die Menschen das Wahlrecht haben."
             },
             {
-                type: "richtige Antwort",
+                type: "your level B2",
                 content: "hier Meinungsfreiheit gilt."
             }
         ]
@@ -24,19 +24,19 @@ var quizQuestions = [
         question: "In Deutschland können Eltern bis zum 14. Lebensjahr ihres Kindes entscheiden, ob es in der Schule am ...",
         answers: [
             {
-                type: "falsche Antwort",
+                type: "your level A1",
                 content: "Geschichtsunterricht teilnimmt."
             },
             {
-                type: "richtige Antwort",
+                type: "your level A2",
                 content: "Religionsunterricht teilnimmt."
             },
             {
-                type: "falsche Antwort",
+                type: "your level B1",
                 content: "Politikunterricht teilnimmt."
             },
             {
-                type: "falsche Antwort",
+                type: "your level B2",
                 content: "Sprachunterricht teilnimmt."
             }
         ]
@@ -45,19 +45,19 @@ var quizQuestions = [
         question: "Deutschland ist ein Rechtsstaat. Was ist damit gemeint?",
         answers: [
             {
-                type: "richtige Antwort",
+                type: "your level A1",
                 content: "Alle Einwohner / Einwohnerinnen und der Staat müssen sich die Gesetze halten."
             },
             {
-                type: "falsche Antwort",
+                type: "your level A2",
                 content: "Der Staat muss sich nicht an die Gesetze halten."
             },
             {
-                type: "falsche Antwort",
+                type: "your level B1",
                 content: "Nur Deutsche müssen die Gesetze befolgen."
             },
             {
-                type: "falsche Antwort",
+                type: "your level B2",
                 content: "Die Gerichte machen die Gesetze."
             }
         ]
